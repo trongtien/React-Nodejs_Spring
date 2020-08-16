0f609adef6bacc9de8514e2a3c69eca00ca940ad
elshine1997 <dangthanhhieu106@gmail.com>
2020-08-16 20:58:08 +0700

    ádsads

--- durian/durian/src/main/java/com/sauriengmientay/loaiTraiCayRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sauriengmientay;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public @Repository interface loaiTraiCayRepository extends CrudRepository<loaiTraiCay, Long> {
-
-}
--- durian/durian/src/main/java/com/sauriengmientay/useraRepository.java
+++
@@ -1,5 +0,0 @@
-package com.sauriengmientay;
-
-public interface useraRepository {
-
-}
